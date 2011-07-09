9b3fb4a028bf28e8d7406d1845b85d86f03199a5
HuardD <HuardD@b0b27590-be07-4c42-8693-6afde9323992>
2011-07-09 17:08:35 +0000

    Ajout de la doc pour Mouse et Rules

--- trunk/src/projetsecret/Mouse.java
+++ trunk/src/projetsecret/Mouse.java
@@ -1,23 +1,24 @@
 package projetsecret;
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
- * @author Clément
+ * Classe statique du contrôle de la souris
  */
 public class Mouse {
     static Coord depart = null;
     static Coord arrive = null;
 
+    /**
+     * Retourne les Coords de la souris au départ
+     * @return Coord
+     */
     static Coord Drag()
     {
         return depart;
     }
 
+    /**
+     * Retourne les Coords de la souris à l'arrivé
+     * @return Coord
+     */
     static Coord Drop()
     {
         return arrive;
