aff75b04955c8da589d196b9fcca645895b85920
riin <riin@b0b27590-be07-4c42-8693-6afde9323992>
2011-07-25 21:15:27 +0000

    Ajout de la classe Player

--- trunk/src/projetsecret/Human.java
+++ trunk/src/projetsecret/Human.java
@@ -3,20 +3,47 @@ package projetsecret ;
 public class Human extends Player
 {
     
-	public Human()
+	public Human(Rules myRules, Color myColor)
 	{
-		maCouleur= null;
-		mesRegles= null ;
+		super.maCouleur= myColor;
+		super.mesRegles= myRules ;
 	}
 	
+        
+        
         public void Think()
         {
-            if(super.mesRegles.Move(maCouleur, Mouse.Drag(), Mouse.Drop()) !=true)
-            {
-                while ((Mouse.Drop()==null)&& (Mouse.Drag()==null))
-                {}
+             Coord temp1,temp2, tempX ;
+             
+        //ce qu'il doit faire c'est tant que le coup n'est pas validé par les règles ... 
+        //il boucle sur 
+        //si la souris Drag alors on regarde si  elle Drop 
+        //et si les 2 marches alors on demande si le coup est bon
+            
+             temp1 = new Coord();
+             temp2 = new Coord();
+            
+            
+            
+            while(super.mesRegles.Move(super.maCouleur,temp1,temp2)== false) 
+            { // On teste rules.move, tant qu'il est faux, c'est à dire que 
+                // le déplacement n'a pas été validé
+                // on continue de demander au joueur un déplacement
+                // on quitte la boucle quand rules.move= vrai
+            
+                if( Mouse.Drag()!=null)
+                {// On teste la prise du pion, si elle est différente de null
+                    // le pion à été pris et on continue
+                    
+                    while(Mouse.Drop()== null)
+                    {// on teste la dépose du pion, avant qu'elle soit valide.
+                        // si elle est valide, alors on passe au joueur suivant
+                        
+                    }
+                }
             
             }
-        }
-
+            
+            
+         }
 }
